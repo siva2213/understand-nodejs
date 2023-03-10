d718418070649bef3920ebe94845efacfb5b415f
Chettebhaktula Siva Krishna <schettebhaktula@altimetrik.com>
2023-03-10 21:40:21 +0530

    routing part

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"/> <button type="submit">Add Product</button></form>')
-})
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
 
-app.post('/product', (req, res, next) => {
-    console.log(JSON.parse(JSON.stringify(req.body)), 'value')
-    res.redirect('/')
-})
-
-app.use('/', (req, res, next) => {
-    res.send('Hello express js')
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
 })
 
 app.listen(3000)
